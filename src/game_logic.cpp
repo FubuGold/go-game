96eee6929aaa8377bc430a5e78e785d7b76edb44
GasmaskChan <soratavn@gmail.com>
2025-10-31 20:30:11 +0700

    chore: remove unused object files and executable

--- src/game_logic.cpp
+++ src/game_logic.cpp
@@ -7,9 +7,8 @@ void reset_game() {
 }
 
 bool check_vaild_move(Move &new_move) {
-    if (std::min(new_move.pos_x, new_move.pos_y) < 0 || std::max(new_move.pos_x, new_move.pos_y) >= 19) return false;
-    if (current_board.get_state(new_move.pos_x, new_move.pos_y) != '.') return false;
+    if (std::min(new_move.pos_x, new_move.pos_y) < 0 || std::max(new_move.pos_x, new_move.pos_y) >= 19) return false; //Checking if this move is inside the board
+    if (current_board.get_state(new_move.pos_x, new_move.pos_y) != '.') return false; //Checking if the current cell is empty
     
     return true;
 }
-
