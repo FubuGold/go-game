4cb9b6253a557046706051b8574453fc4da931c8
FubuGold <trangiahuy211207@gmail.com>
2025-11-03 08:02:49 +0700

    feat: test doxygen for documentation

--- include/game_scoring.h
+++ include/game_scoring.h
@@ -4,6 +4,17 @@
 #include "game_logic.h"
 #include <utility>
 
+/**
+ * @brief Calculate current score from a board state
+ * 
+ * This function take a board state and calculate the score
+ * 
+ * @param board Current board state to calculate
+ * 
+ * @return std::pair<int,int> 
+ * 
+ * {black score, white score}
+ */
 std::pair<int,int> scoring(const Board &board);
 
 #endif // GAME_SCORING
