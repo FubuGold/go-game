8e9f5f446bffec645d44173653d37b0585c0783f
FubuGold <trangiahuy211207@gmail.com>
2025-11-07 17:18:48 +0700

    feat: add input for other functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <windows.h>
 
 #include "../include/game_logic.h"
 
@@ -10,37 +12,72 @@ void debug() {
     std::cerr << current_board.save_game();
 }
 
+void to_lower_all(std::string &s) {
+    for (int i=0;i<s.size();i++) s[i] = tolower(s[i]);
+}
+
+bool validate_user_input(std::string s) {
+    to_lower_all(s);
+    if (s == "m" || s == "u" || s == "s" || s == "l") return true;
+    return false; 
+}
+
 int main() {
     bool turn = 1;
 
-    // while (true) {
-    //     current_board.print_board();
-        
-    //     if (!current_board.check_empty_move_list()) {
-    //         std::cerr << "Hash value of the previous move: " << current_board.get_board_hash() << '\n';
-    //         std::cout << "Do you want to undo the last move? 1 - Yes, 0 - No ";
-    //         bool undo_or_not;
-    //         std::cin >> undo_or_not;
-    //         if (undo_or_not) {
-    //             current_board.undo_move();
-    //             system("cls");
-    //             continue;
-    //         }
-    //     }
-        
-    //     std::cout << "Current turn: " << (turn ? "Black" : "White") << std::endl;
-    //     int pos_x, pos_y;
-    //     do {
-    //         std::cout << "Input the coordinate where you want to place your stone: ";
-    //         std::cin >> pos_x >> pos_y;
-    //         if (!check_vaild_move(Move(pos_x, pos_y, turn ? 'X' : 'O'))) {
-    //             std::cout << "Your move is invaild, please try again!" << std::endl;
-    //         }
-    //         else break;
-    //     } while (true);
+    while (true) {
+        current_board.print_board();
 
-    //     turn ^= 1;
-    //     system("cls");
-    // }
+        std::cout << "Current turn: " << (turn ? "Black" : "White") << std::endl;
+        std::cout << "Enter one of the command below (lowercase or uppercase):\n";
+        std::cout << "m: play move for current player\n";
+        std::cout << "u: undo previous move\n";
+        std::cout << "s: current game\n";
+        std::cout << "l: load game in save file\n";
+        std::cout << "Input: ";
+        std::cout.flush();
+
+        std::string inp;
+        std::cin >> inp;
+        
+        std::cin.ignore();
+
+        while (!validate_user_input(inp)) {
+            std::cout << "Invalid input. Please try again.\n";
+            std::cout << "Input: ";
+            std::cout.flush();
+        }
+
+        to_lower_all(inp);
+
+        if (inp == "m") {
+            std::cout << "Input coordinate: ";
+            int pos_x,pos_y; std::cin >> pos_x >> pos_y;
+            while (!check_vaild_move(Move(pos_x,pos_y))) {
+                std::cout << "Invaild move. Try again\n";
+                std::cout << "Input coordinate: ";
+                std::cout.flush();
+            }
+            std::cin.ignore();
+            turn ^= 1;
+        }
+        else if (inp == "u") {
+
+        }
+        else if (inp == "s") {
+            if (current_board.save_game()) continue;
+            std::cout << "Save game failed\n";
+            std::cout << "Try again after 5 second" << std::endl;
+            Sleep(5000);
+        }
+        else if (inp == "l") {
+            if(current_board.load_game()) continue;
+            std::cout << "Load game failed\n";
+            std::cout << "Try again after 5 seconds" << std::endl;
+            Sleep(5000);
+        }
+
+        system("cls");
+    }
     return 0;
 }
