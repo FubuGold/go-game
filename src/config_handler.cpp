bed2c73752b0ceba198acd60b365eb1645ec1521
Gasmask-Chan <soratavn@gmail.com>
2025-12-04 15:27:22 +0700

    feat: finish all buttons on gameplay frame, still need to setup AI and pop up

--- src/config_handler.cpp
+++ src/config_handler.cpp
@@ -11,7 +11,8 @@ namespace Config {
         sf::Music("audio/song/song_3.mp3")
     };
     
-    int prev_save = 0;
+    bool music_on = true;
+
     int selected_theme = 0;
     int selected_bgm = 0;
     int audio_volume[3] = {100,100,100};
@@ -25,7 +26,6 @@ namespace Config {
         std::ifstream inp("data/config.json");
         inp >> j;
 
-        j.at("prev_save").get_to(prev_save);
         j.at("theme").get_to(selected_theme);
         j.at("bgm").get_to(selected_bgm);
         j.at("audio_volume").get_to(audio_volume);
@@ -40,7 +40,6 @@ namespace Config {
     void save_config() {
         json j;
 
-        j["prev_save"] = prev_save;
         j["theme"] = selected_theme;
         j["bgm"] = selected_bgm;
         j["audio_volume"] = audio_volume;
