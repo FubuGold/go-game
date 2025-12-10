9dcd0961395b8e4e58f92383a9dd6204f969cdc2
FubuGold <trangiahuy211207@gmail.com>
2025-12-10 09:03:36 +0700

    chore: change function name to match the content

--- include/config_handler.h
+++ include/config_handler.h
@@ -37,13 +37,12 @@ namespace Config {
     extern int selected_bgm;
     extern int audio_volume[3]; // General - Music - SFX
 
-    float cal_music_vol(int id);
+    float cal_volume(int id);
 
     void load_config();
     void save_config();
 
     void change_music(int new_bgm);
-    void change_theme(int new_theme);
     void update_volume();
 }
 
